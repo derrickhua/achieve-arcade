59b558b39eaed94ee31138a9876e0d94f2aa0d87
derrickhua <derrickhua6@gmail.com>
2024-06-11 19:41:39 -0400

    Goals no data addition

--- frontend/src/components/pages/GoalKnight.tsx
+++ frontend/src/components/pages/GoalKnight.tsx
@@ -7,6 +7,7 @@ import EditGoalForm from '../forms/EditGoal';
 import DeleteGoalForm from '../forms/DeleteGoal';
 import { Goal } from '@/lib/goals';
 import Image from 'next/image';
+
 const GoalKnight: React.FC<{ fetchCoins: () => void }> = ({ fetchCoins }) => {
   const [goals, setGoals] = useState([]);
   const [filter, setFilter] = useState('All');
@@ -87,7 +88,7 @@ const GoalKnight: React.FC<{ fetchCoins: () => void }> = ({ fetchCoins }) => {
         </div>
         <AddButton name="ADD GOAL" onClick={openAddGoalForm} />
       </div>
-      <div className="flex flex-wrap justify-between max-w-[1800px] w-full">
+      <div className={`flex flex-wrap justify-between max-w-[1800px] w-full ${filteredGoals.length === 0 ? 'h-full' : ''}`}>
         {filteredGoals.length > 0 ? (
           filteredGoals.map(goal => (
             <div key={goal._id} className="w-full md:w-1/2 p-4">
@@ -100,13 +101,32 @@ const GoalKnight: React.FC<{ fetchCoins: () => void }> = ({ fetchCoins }) => {
             </div>
           ))
         ) : (
-          <div className="w-full bg-[#BDBDBD] rounded-lg p-4 md:p-6 h-full justify-center flex flex-col items-center">
-            <div className="w-full flex justify-center items-center h-2/3 gap-[60px]">
-              <Image src="/icons/no-data/zombie-run.gif" alt="No Data Gif 2" width={160} height={180} style={{ imageRendering: 'pixelated' }} />
-              <Image src="/icons/no-data/wizard-run.gif" alt="No Data Gif 1" className="mt-[50px]" width={80} height={140} style={{ imageRendering: 'pixelated' }} />
-            </div>
-            <div className="flex justify-center h-1/3 text-center text-[40px] text-black">{`NO GOALS???`}</div>
-          </div>
+<div className="w-full rounded-lg p-4 md:p-6 h-full flex flex-col items-center justify-center relative">
+  {/* Adding multiple images with absolute positioning */}
+  <Image src="/icons/goal-knight/goblins/wood-tower.png" alt="Wood Tower" width={250} height={100} className="absolute top-[100px] left-[100px]" />
+  <Image src="/icons/goal-knight/goblins/wood-tower.png" alt="Wood Tower" width={250} height={100} className="absolute top-[100px] right-[100px]" />
+  <Image src="/icons/goal-knight/goblins/wood-tower.png" alt="Wood Tower" width={250} height={100} className="absolute bottom-[100px] left-[100px]" />
+  <Image src="/icons/goal-knight/goblins/wood-tower.png" alt="Wood Tower" width={250} height={100} className="absolute bottom-[100px] right-[100px]" />
+  
+  <Image src="/icons/goal-knight/goblins/tnt.gif" alt="TNT" width={192} height={192} className="absolute top-[20%] left-[20%]" />
+  <Image src="/icons/goal-knight/goblins/tnt.gif" alt="TNT" width={192} height={192} className="absolute top-[3%] right-[7%] transform scale-x-[-1]" />
+  <Image src="/icons/goal-knight/goblins/tnt.gif" alt="TNT" width={192} height={192} className="absolute bottom-[20%] left-[50%]" />
+
+  <Image src="/icons/goal-knight/goblins/side-torch.gif" alt="Side Torch" width={192} height={192} className="absolute top-[35%] left-[11%]" />
+  <Image src="/icons/goal-knight/goblins/front-torch.gif" alt="Front Torch" width={192} height={192} className="absolute bottom-[30%] left-[67%]" />
+
+  <Image src="/icons/goal-knight/goblins/goblin-house.png" alt="Goblin House" width={100} height={100} className="absolute top-[30%] left-[30%]" />
+  <Image src="/icons/goal-knight/goblins/goblin-house.png" alt="Goblin House" width={100} height={100} className="absolute top-[30%] right-[30%]" />
+  <Image src="/icons/goal-knight/goblins/goblin-house.png" alt="Goblin House" width={100} height={100} className="absolute bottom-[30%] right-[60%]" />
+
+  <Image src="/icons/goal-knight/goblins/house-destroyed.png" alt="House Destroyed" width={100} height={100} className="absolute top-[60%] left-[70%]" />
+  <Image src="/icons/goal-knight/goblins/house-destroyed.png" alt="House Destroyed" width={100} height={100} className="absolute top-[50%] right-[20%]" />
+
+  <Image src="/icons/goal-knight/goblins/tower-destroyed.png" alt="Tower Destroyed" width={100} height={100} className="absolute bottom-[50%] left-[20%]" />
+  <Image src="/icons/goal-knight/goblins/tower-destroyed.png" alt="Tower Destroyed" width={100} height={100} className="absolute bottom-[50%] right-[20%]" />
+
+  <div className="flex justify-center text-center text-[96px] text-[#B65555]">{`NO GOALS???`}</div>
+</div>
         )}
       </div>
       {isAddGoalFormOpen && (
