1a310200acf396ca0bfad2475c67926b49b9beb7
derrickhua <derrickhua6@gmail.com>
2024-06-21 20:17:09 -0400

    4th try

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,16 @@
+import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    // Other configurations
-  };
-  
-  export default nextConfig;
-  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Add the CaseSensitivePathsPlugin to the plugins array
+    config.plugins.push(new CaseSensitivePathsPlugin());
+
+    return config;
+  },
+};
+
+export default nextConfig;
