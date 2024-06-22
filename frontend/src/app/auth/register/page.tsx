b5462b6d455f4325335bcd877362ee536cc2fcba
derrickhua <derrickhua6@gmail.com>
2024-06-22 01:47:59 -0400

    possible win

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { Button } from "../../../components/ui/button"
 import { Input } from "../../../components/ui/input"
 import { Label } from "../../../components/ui/label"
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { signIn } from "next-auth/react";
 
 
